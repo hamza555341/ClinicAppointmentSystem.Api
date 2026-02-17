41e2005e3ea3826c6e33ad8c2e65a2ed6d8ce498
A C E <A C E@HAMZA>
2026-02-17 02:11:58 +0200

    Implement repository pattern in persistence layer

---
+++ ClinicAppointment.Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,55 @@
+﻿using ClinicAppointment.Domain.Entites;
+using ClinicAppointment.Domain.Interfaces;
+using ClinicAppointment.Persistence.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicAppointment.Persistence.Repositories
+{
+    public class GenericRepository<TEntity, TKey> :
+        IGenericRepository<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        private readonly ClinicAppointmentsDbContext _dbContext;
+
+        public GenericRepository(ClinicAppointmentsDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task AddAsync(TEntity entity)=>
+            await _dbContext.Set<TEntity>().AddAsync(entity);
+
+
+        public void Delete(TEntity entity)=>
+                 _dbContext.Set<TEntity>().Remove(entity);
+
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>>? criteria = null, params Expression<Func<TEntity, object>>[] includes)
+        {
+            IQueryable<TEntity> query = _dbContext.Set<TEntity>();
+
+            if (criteria != null)
+                query = query.Where(criteria);
+
+            if (includes != null && includes.Any())
+                foreach (var include in includes)
+                    query = query.Include(include);
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<TEntity?> GetByIdAsync(TKey id)=>
+            await _dbContext.Set<TEntity>().FindAsync(id);
+
+
+        public void Update(TEntity entity)=>
+                 _dbContext.Set<TEntity>().Update(entity);
+
+    }
+}
---
+++ ClinicAppointment.Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,42 @@
+﻿using ClinicAppointment.Domain.Entites;
+using ClinicAppointment.Domain.Interfaces;
+using ClinicAppointment.Persistence.DbContexts;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicAppointment.Persistence.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ClinicAppointmentsDbContext _dbContext;
+        private readonly Dictionary<Type, object> _repositories = [];
+        public UnitOfWork(ClinicAppointmentsDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        public IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>() where TEntity : BaseEntity<TKey>
+        {
+          var EntityType= typeof(TEntity);
+
+            if (_repositories.TryGetValue(EntityType, out object? Repo))
+                return (IGenericRepository<TEntity,TKey>)Repo;
+
+            var NewRepo = new GenericRepository<TEntity, TKey>(_dbContext);
+
+            _repositories[EntityType] = NewRepo;
+            return NewRepo; 
+
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+           return await _dbContext.SaveChangesAsync(); 
+        }
+    }
+}
