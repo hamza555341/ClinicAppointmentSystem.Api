50c4668df4353e06230bd1fa5b139538168dd832
A C E <A C E@HAMZA>
2026-02-17 02:17:40 +0200

    Add background job infrastructure

---
+++ ClinicAppointmentSystemApi/BackgroundJobs/HangFireJobService.cs
@@ -0,0 +1,15 @@
+﻿using ClinicAppointment.Service.Abstraction;
+using Hangfire;
+
+namespace ClinicAppointmentSystemApi.BackgroundJobs
+{
+    public class HangfireJobService : IBackgroundJobService
+    {
+        public void ScheduleAppointmentCompletion(int appointmentId, DateTime runAt)
+        {
+            BackgroundJob.Schedule<IAppointmentService>(
+                service => service.AutoCompleteAppointmentAsync(appointmentId),
+                runAt);
+        }
+    }
+}
